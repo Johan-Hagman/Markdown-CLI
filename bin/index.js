c24f33b5bcdaae8319bd8a01f6fe2344b09fbf1b
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-02-11 13:16:20 +0100

    Added first directory search logic

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,22 @@
-console.log("hello");
+import fs from "fs";
+import path from "path";
+
+const directoryName = process.argv[2];
+
+if (!directoryName) {
+  console.log("Please Enter A Directory!");
+  process.exit(1);
+}
+
+const directoryPath = path.resolve(directoryName);
+
+fs.stat(directoryPath, (err, stats) => {
+  if (err) {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+  }
+  if (!stats.isDirectory()) {
+    console.log(`${directoryName} is not a directory`);
+  }
+  console.log(`Directory ${directoryName} exists!`);
+});
